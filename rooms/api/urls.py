f4541e47524994092abe0e97c02096dd07acc294
William Bernal <wbernal2@huskers.unl.edu>
2025-06-10 15:57:23 -0500

    Added Request for creating room
    api/create/

--- rooms/api/urls.py
+++ rooms/api/urls.py
@@ -2,7 +2,8 @@ from .views import RoomView, CreateRoom, JoinRoom
 from django.urls import path
 
 urlpatterns = [
-    path('home', RoomView.as_view()),
-    path('create', CreateRoom.as_view()),
-    path('join', JoinRoom.as_view()),
+    path('home/', RoomView.as_view(), name='home'),
+    path('create/', CreateRoom.as_view(), name='create-room'),
+    path('join/', JoinRoom.as_view(), name='join-room'),
 ]
+
